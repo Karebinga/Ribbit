c2d54a844ba7ea7c215ad05dfd616b272f347ae5
Karebinga <karebinga@gmail.com>
2021-05-16 14:38:11 +0300

    Add DotTween

---
+++ Ribbit!/Assets/Tongue.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Tongue : MonoBehaviour
+{
+    private Camera _mainCamera;
+    private Collider2D _collider;
+
+    private int _length;
+    private int _strength;
+    private int _fishCount;
+
+    private bool _canMove;
+}
